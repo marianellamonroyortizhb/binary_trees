d3a8969934d7a03b7a75c0aac043f7a01cf6e7d9
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-12 04:13:55 +0000

    Add ; in file 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t right_size = 0;
 
 	if (!tree)
-		return (0)
+		return (0);
 
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
