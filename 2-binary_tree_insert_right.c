73e85df47daa1d0f9849f2fc36476b513bda706e
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-12 02:53:48 +0000

    add 2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - New leaf lik a child.
+ * @parent: Pointer
+ * @value: Integer
+ *
+ * Return: New leaft .
+ **/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *rightNode = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	rightNode = binary_tree_node(parent, value);
+
+	if (parent->right)
+	{
+		rightNode->right = parent->right;
+		parent->right->parent = rightNode;
+	}
+	parent->right = rightNode;
+
+	return (rightNode);
+}
