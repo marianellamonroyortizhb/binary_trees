6cca75bba13ea41b8a7b5af50ca2be79945645d7
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-12 03:49:14 +0000

    Add 6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Transversal preorder.c
+ *
+ * @tree: Tree
+ * @func: Function
+ * Return: Evaluation
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
