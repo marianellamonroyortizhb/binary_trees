23b3b290daeb7942fad845279f0fc66d3a3b7c34
marianellamonroyortizhb <1880@holbertonschool.com>
2021-03-12 04:55:11 +0000

    Change if in file 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	else if (left_height < right_height)
+	if (left_height < right_height)
 		return (right_height + 1);
 
 	return (left_height + 1);
